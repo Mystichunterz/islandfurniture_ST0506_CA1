f99b7d30921c5d3f49cf67cbd06ee505e58faf33
DNJKboop <declanngjk.22@ichat.sp.edu.sg>
2024-01-04 17:03:27 +0800

    BUg fixes on promotion page

--- model/deliveryDetailsModel.js
+++ model/deliveryDetailsModel.js
@@ -2,7 +2,7 @@ var db = require('./databaseConfig.js');
 var DeliveryDetails = require('./deliverydetails.js')
 var deliveryDetailsDB = {
     addDeliveryDetails: function (data) {
-        return new Promise( ( resolve, reject ) => {
+        return new Promise((resolve, reject) => {
             var conn = db.getConnection();
             conn.connect(function (err) {
                 if (err) {
@@ -11,14 +11,14 @@ var deliveryDetailsDB = {
                     return reject(err);
                 }
                 else {
-                    var sqlArgs = [data.memberId,data.address,data.postalCode,data.contactNum,data.name,data.salesRecordId];
+                    var sqlArgs = [data.memberId, data.address, data.postalCode, data.contactNum, data.name, data.salesRecordId];
                     var sql = 'INSERT INTO deliverydetailsentity (MEMBER_ID,DELIVERY_ADDRESS,POSTAL_CODE,CONTACT,NAME,SALERECORD_ID) VALUES (?,?,?,?,?,?)';
                     conn.query(sql, sqlArgs, function (err, result) {
                         if (err) {
                             conn.end();
                             return reject(err);
                         } else {
-                            if(result.affectedRows > 0) {
+                            if (result.affectedRows > 0) {
                                 conn.end();
                                 return resolve(true);
                             }
